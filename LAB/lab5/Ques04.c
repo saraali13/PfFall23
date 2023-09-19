c89f215bcf31de4167408326dab040d3e48e4822
saraali13 <142868034+saraali13@users.noreply.github.com>
2023-09-19 16:36:28 +0100

    Create Ques04.c

---
+++ LAB/lab5/Ques04.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(){
+    int n1;
+    int n2;
+    int ans;
+    char op;
+    printf("enter first number\n");
+    scanf("%d",&n1);
+    printf("enter second number\n");
+    scanf("%d",&n2);
+    printf("choose the operator");
+    scanf("%c",&op);
+    switch ('op')
+    {
+        case '+':
+         printf("ans=%d",n1+n2);
+         break;
+        case '-':
+         printf("ans=%d",n1-n2);
+         break;
+       case '*':
+         printf("ans=%d",n1*n2);
+         break;
+       case '/':
+         printf("ans=%d",n1/n2);
+         break;
+      default:
+        printf("error");
+        break;
+    }// end switch
+    return 1;
+}// end main
+    
+    
+
+
+    
+    
+
