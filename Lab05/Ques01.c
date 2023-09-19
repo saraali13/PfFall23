feb1b12169356ed0e4df1eb2c66643572517a6ab
saraali13 <142868034+saraali13@users.noreply.github.com>
2023-09-19 09:00:11 +0500

    Create Ques01.c

---
+++ Lab05/Ques01.c
@@ -0,0 +1,14 @@
+# labtask 01:
+#include <stdio.h>
+#include <math.h>
+void main() {
+
+  int n;
+  printf("enter a number");
+  scanf("%d",&n);
+  if(n%3 == 0)
+   { printf("this number is a multiple of 3");
+    } //end if
+  else
+   printf("this number is not a multiple of 3");
+} //end main
