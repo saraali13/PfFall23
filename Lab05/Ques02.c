500cf932a8f1d0174734e69afe4a1f78171f54a4
saraali13 <142868034+saraali13@users.noreply.github.com>
2023-09-19 09:23:36 +0500

    Create Ques02.c

---
+++ Lab05/Ques02.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int hour;
+    printf("enter hour \n");
+    scanf("%d",&hour);
+    if (hour>=5 && hour<= 11)
+      { printf("Good Morning");
+       }
+   else if (hour>=12 && hour<=18)
+      { printf("Good Evening");
+        }
+   else if (hour >18 && hour<=24)
+      { printf("Good Night");
+        }
+return 1;
+} // end main
+    
