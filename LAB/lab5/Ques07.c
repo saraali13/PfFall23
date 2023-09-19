e5dc5e322b5ec3062189ba517fc74fd2a7cf32a1
saraali13 <142868034+saraali13@users.noreply.github.com>
2023-09-19 18:01:29 +0100

    Create Ques07.c

---
+++ LAB/lab5/Ques07.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+int main () {
+    int op;
+    int sale;
+    int sa;
+    int amount;
+    printf("enter orignal price");
+    scanf("%d",&op);
+    if ((op >= 1999)||(op>=6000))
+     { sale = 50 ;
+       amount =(sale*op)/100 ;
+       sa=op-amount ;
+     }// end if
+    else if ((op >= 2000)&&(op<=4000))
+     { sale = 20 ;
+       amount =(sale*op)/100 ;
+       sa=op-amount ;
+     }// end else if
+    else if ((op >= 4001)&&(op<=6000))
+     { sale = 30 ;
+       amount =(sale*op)/100 ;
+       sa=op-amount ;
+     }//end else if
+    printf("orignal price:%d\n",op);
+    printf("total amount after sale:%d\n",amount);
+    printf("saved amount:%d\n",sa);
+    return 1;
+}// end main
