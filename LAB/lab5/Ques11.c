41ef8cfe20464039cd23cd5c2f0f30e76fbe45f9
saraali13 <142868034+saraali13@users.noreply.github.com>
2023-09-20 18:48:06 +0100

    Update Ques11.c

--- LAB/lab5/Ques11.c
+++ LAB/lab5/Ques11.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+int main() {
+    int bin,bit,higher_nibble=0,lower_nibble,j,i=0,ans ;
+    printf("enter 8 bit binary number:");
+    scanf("%d",&bin);
+    if (bin<10)
+    {
+        printf("enter atleast two digits");
+    }//end if
+    lower_nibble=bin/10000 ;
+    while (i<4)
+    {
+        bit=bin%10;
+        bin=bin/10;
+        j=i;
+        while(j>0)
+        {
+            bit=bit*10;
+            j=j-1 ;
+        }//end while
+        higher_nibble=higher_nibble+bit ;
+        i=i+1 ;
+    }//end while
+        higher_nibble=higher_nibble*10000 ;
+        ans=higher_nibble + lower_nibble ;
+        printf("%08d",ans);
+}//end main
