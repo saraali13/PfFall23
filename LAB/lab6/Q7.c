9a34890aaba7f74b1ef2e83c14f29519e224860d
saraali13 <142868034+saraali13@users.noreply.github.com>
2023-10-07 13:30:11 +0100

    Create Q7.c

---
+++ LAB/lab6/Q7.c
@@ -0,0 +1,22 @@
+/* 
+Name:Syeda Sara Ali
+Date: 3-oct-2023 
+Description: sum of num in an array 
+*/
+#include <stdio.h>
+
+int main() {
+	
+    int x, sum=0 ,n;
+    printf("enter size of array");
+    scanf("%d",n);
+    int arr[n];
+   for (int i=0;i<n;i++)
+      {
+    	printf("enter the value of %d array\n",i);
+    	scanf("%d",x);
+    	sum=sum+x;
+      }// end for
+    printf("sum=%d",sum);            
+    return 0;
+}//end main
