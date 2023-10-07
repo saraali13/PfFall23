2bbfac1af34032f998c7c617dd3b87a3750aab3c
saraali13 <142868034+saraali13@users.noreply.github.com>
2023-10-07 13:57:05 +0100

    Create Q8.c

---
+++ LAB/lab6/Q8.c
@@ -0,0 +1,28 @@
+/* 
+Name:Syeda Sara Ali
+Date: 3-oct-2023 
+Description: sum of num in an array 
+*/
+#include <stdio.h>
+
+int main() {
+	
+    int n;
+    printf("enter size of array");
+    scanf("%d",&n);
+    int arr[n];
+    for (int i=0;i<n;i++)
+        {  
+    	printf("enter the value of %d array\n",i);
+    	scanf("%d",arr[i]);
+    	printf("the value of %d array is %d\n",i,arr[i]);
+        }// end for 
+    //n=9;
+    //int arr[9]={1,2,3,4,5,6,7,8,9} ;
+    for (int i=n;i>0;i--)
+    {
+    	
+    	printf("%d",arr[i]);
+	}  // end for    
+    return 0;
+}//end main
