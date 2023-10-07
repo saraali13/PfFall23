d0a6a0647a74521476b574cebcc3373a75811452
saraali13 <142868034+saraali13@users.noreply.github.com>
2023-10-07 13:57:29 +0100

    Update Q7.c

--- LAB/lab6/Q7.c
+++ LAB/lab6/Q7.c
@@ -9,12 +9,12 @@ int main() {
 	
     int x, sum=0 ,n;
     printf("enter size of array");
-    scanf("%d",n);
+    scanf("%d",&n);
     int arr[n];
    for (int i=0;i<n;i++)
       {
     	printf("enter the value of %d array\n",i);
-    	scanf("%d",x);
+    	scanf("%d",&x);
     	sum=sum+x;
       }// end for
     printf("sum=%d",sum);            
