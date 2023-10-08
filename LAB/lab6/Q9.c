5bc9c7353ef0ba30e7ad2f8059a5da7923999525
saraali13 <142868034+saraali13@users.noreply.github.com>
2023-10-08 13:38:49 +0100

    Update Q9.c

--- LAB/lab6/Q9.c
+++ LAB/lab6/Q9.c
@@ -1,21 +1,32 @@
 /* 
 Name:Syeda Sara Ali
 Date: 3-oct-2023 
-Description: sum of num in an array 
+Description:Max and Min num in an array
 */
 #include <stdio.h>
 
 int main() {
+	int n,sum=0,max,min;
+	printf("enter size of array you want");
+	scanf("%d",&n);
+	int array[n];
+	for(int i = 0; i < n; i++) {
+		printf("enter %d array items: ",i);
+		scanf("%d",&array[i]);
+	}// end for
+	max=min=array[0];
+	for(int i = 0;i<n;i++) {
+	//condition to check max values
+		if(array[i]>max) {
+			max=array[i];
+		}// end if
+	//condition to check min values
+		if(array[i]<min) {
+			min=array[i];
+		}// end if
+	}// end for
 	
-    int x, sum=0 ,n;
-    printf("enter size of array");
-    scanf("%d",&n);
-    int arr[n];
-   for (int i=0;i<n;i++)
-      {
-    	scanf("%d",&arr[i]);
-    	sum=sum+arr[i];
-      }// end for
-   printf("sum=%d",sum);            
-    return 0;
+	printf("the smallest value in the array is: %d\n",min);
+	printf("the largest value in the array is: %d\n",max);
+	return 1;int 
 }//end main
