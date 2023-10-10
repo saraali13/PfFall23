8d6a5267f115c6959bb6bfdaf5831db96e8c30e5
S.Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2023-10-10 09:37:00 +0500

    Create Q3.c

---
+++ LAB/lab7/Q3.c
@@ -0,0 +1,21 @@
+/* 
+Name:Syeda Sara Ali
+Date: 3-oct-2023 
+Description: rotate the elements of an array 
+*/
+#include <stdio.h>
+
+int main() {
+	
+    int n , sum=0;
+    printf("enter size of array");
+    scanf("%d",&n);
+    int arr[n];
+    for (int i=0;i<n;i++)
+    {  
+    	scanf("%d",&arr[i]);
+    	sum=sum+arr[i];
+    }// end for 
+    printf("%d",sum);
+    return 1;
+}// end main
