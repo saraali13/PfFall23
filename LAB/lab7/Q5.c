33b773c450da1dcc4357b485413fbc20016019eb
S.Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2023-10-18 20:27:51 +0100

    Update Q5.c

--- LAB/lab7/Q5.c
+++ LAB/lab7/Q5.c
@@ -23,122 +23,100 @@ Description: OUTPUT
    ** **
 ***** *****
 **/
-#include <stdio.h>
-void cap(int n)
-{
-    int a,b;
-    for(int i=0;i<n;i++)
-	{
-        for(int j =0;j<n*2;j++)
-		{
-            if(j<n)
-			{
-                printf(" ");
-            }//end nested if
-			else printf("*");
-        }// end nested for
-        printf("\n");
-    }// end for
-    for(int i=0;i<n*3-2;i++)
-	{
-        if(i<2)
-		{
-            printf(" ");
-        }// end if
-	   else printf("*");
-    }// end for
-    printf("\n");
-}// end void cap
 
-void face(int n)
-{
-    for(int i=0;i<n*2;i++)
-	{
-        if(i==n || i == n*2-1)
-		{
-            printf("*");
-        }// end if
-		else printf(" ");
-    }// end for
-    printf("\n");
-    for(int i=0;i<n*2-2;i++)
-	{
-        if(i==n*2-3)
-		  printf("|");
-        else printf(" ");
-    }// end for
-    printf("\n");
-    for(int j=0;j<2;j++)
-	{
-        for(int i=0;i<n+2;i++)
-		{
-            if(j==0)
-			{
-                if(i==n+1)
-				{
-                    printf(" (_)");
-                    printf("\n\n");
-                }// end nested if
-                else printf(" ");
-                
-            }// end if
-			else 
-			{
-                if(i==n+1)printf("| |");
-                else printf(" ");
-            }// end else
-        }// end nested for
-    }//end for
-    printf("\n");
-}//end void face
+#include<stdio.h>
 
-void body(int n)
-{
-    for(int i =0;i<n*3;i++)
-	{
-        printf("*");
-    }// end for
-    printf("\n");
-    for(int j=0;j<n-2;j++)
-	{
-        for(int i=0;i<n*3;i++)
-		{
-            if(i<n||i>=n*2)
+main() {
+	
+	//function for the hat
+	char hat() {
+		int i,j,k,z;
+		for(z = 0;z<3;z++) {
+			for(j = 0;j<4;j++) {
+			   printf(" ");
+		    }
+		    for(i = 0;i<5;i++) {
+			   printf("*");
+		    }
+		    printf("\n");
+		}
+		printf(" ");
+		for(k = 0;k<11;k++) {
 			printf("*");
-           else printf(" ");
-        }// end nested for
-        printf("\n");
-    }// end for
-}//end void body
-void legs(int n)
-{
-    for(int j=0;j<n;j++)
-	{
-        for(int i=0;i<=n*2;i++)
-		{
-            if(i<=n-1 || i==n*2-3 )
-			{
-                printf(" ");
-            }//end if
-		else printf("*");
-        }// end nested for
-        printf("\n");
-    }// end for
-    for(int i=0;i<n*3;i++)
-	{
-        if(i==n*2-3) printf(" ");
-        else printf("*");
-    }// ed for
-}// end void(leg)
-int main() 
-{
-    int a,b,c,i,j,k,n;
-    printf("Enter a number = ");
-    scanf("%d",&n);
-    
-    cap(n);
-    face(n);
-    body(n);
-    legs(n);
-    return 0;
+		}
+		printf("\n");
+	}//end hat
+	
+	//function for the face
+	char face() {
+		int i;
+		for(i = 0;i<4;i++) {
+			printf(" ");
+		}
+		printf("*");
+		for(i = 0;i<3;i++) {
+			printf(" ");
+		}
+		printf("*\n");
+		
+		for(i=0;i<6;i++) {
+			printf(" ");
+		}
+		printf("|\n");
+		
+		for(i = 0;i<5;i++) {
+			printf(" ");
+		}
+		printf("\\"); //double slash cancels out the escape sequence
+		printf("_");
+		printf("/\n");
+		
+		printf("\n");
+		
+		for(i = 0;i<5;i++) {
+			printf(" ");
+		}
+		printf("|");
+		printf(" ");
+		printf("|\n");
+		
+	}//end face
+	
+	//function for the torso
+	char body() {
+		int i,y;
+		for(i=0;i<13;i++) {
+			printf("*");
+		}
+		printf("\n");
+		//hear instead of loops used multiple characters as a single character
+		for(i=0;i<4;i++) {
+			printf("**");
+			printf("  ");
+			printf("*****");
+			printf("  ");
+			printf("**\n");
+		}
+		
+		printf("    ");
+		printf("*****\n");
+		
+		for(i=0;i<5;i++) {
+			printf("    ");
+			printf("**");
+			printf(" ");
+			printf("**\n");
+		}
+		
+		printf(" ");
+		printf("*****");
+		printf(" ");
+		printf("*****");
+	}//end body
+	
+	//calling all the functions in the correct order
+	hat();
+	face();
+	body();
+	
 }//end main
