5c84c454d973cb7a0f64085dfaca68bbae204d6d
S.Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2023-10-24 08:25:35 +0500

    Update Q01.c

--- LAB/lab8/Q01.c
+++ LAB/lab8/Q01.c
@@ -1,1 +1,21 @@
-
+/**Name:S.Sara Ali
+*Date:24-10-23
+*Des:A user defined function which swapa two integers 
+***/
+#include <stdio.h>
+void swap(int a,int b)
+{
+	printf("enter two integers\n");
+	scanf("%d %d",&a,&b);
+	int x;
+	x=a;
+	a=b;
+	b=x;
+	printf("a=%d\n",a);
+	printf("b=%d\n",b);
+}// end swap
+int main ()
+{   
+    int a,b;
+	swap(a,b);
+}//end main
