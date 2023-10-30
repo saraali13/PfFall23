28fc88002bda7c6704658ff383b6b3cda396bf59
S.Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2023-10-30 19:14:27 +0000

    Update Q3.c

--- Assignment2/Q3/Q3.c
+++ Assignment2/Q3/Q3.c
@@ -1,1 +1,124 @@
+/***
+*Name:S.Sara Ali
+*Des: A program for a airport system to check for flight availabilty and ticket prices with 4 main functionalities
+*Date: 26-october-23
+***/
 
+#include <stdio.h>
+
+int main() {
+    int mr[5][2] = {
+        {1, 300},
+        {1, 320},
+        {0, 0},
+        {1, 380},
+        {1, 375}  };//morning flight availability and ticket prices
+    
+    int ev[5][2] = {
+        {0, 0},
+        {1, 310},
+        {1, 280},
+        {0, 0},
+        {1, 400}  };//evening flight availability and ticket prices
+
+    int op, min, day,i,dn;
+    int availableDaysMr = 0;
+    int availableDaysEv = 0;
+    char t1[] = "morning";
+    char t2[] = "evening";
+
+    printf("Enter your choice (1-4):\n ");
+    printf("1. most convenient day and time slot\n");
+    printf("2. the best morning flight options\n");
+    printf("3. the best evening flight options\n");
+    printf("4. Check availability and price for a specific day\n");
+    scanf("%d", &op);
+
+    switch (op) {
+        case 1://most most convenient day and time
+            for (i = 0; i < 5; i++) {
+                if (mr[i][0] == 1) {
+                    availableDaysMr++;
+                }//end if
+                if (ev[i][0] == 1) {
+                    availableDaysEv++;
+                }//end if
+            }//end for
+            if (availableDaysMr> availableDaysEv) 
+			{
+                min = mr[0][1];
+                day = 1;
+                for ( i = 0; i < 5; i++) {
+                    if (mr[i][1] < min && mr[i][0] == 1) {
+                        min = mr[i][1];
+                        day = i + 1;
+                    }//end if
+                }//end for
+                printf("The preferred day and time is %d , %s\n", day, t1);
+            } //end if
+			else {
+                min = ev[0][1];
+                day = 1;
+                for ( i = 0; i < 5; i++) {
+                    if (ev[i][1] < min && ev[i][0] == 1) {
+                        min = ev[i][1];
+                        day = i + 1;
+                    }//end if
+                }//end for
+                printf("The preferred day and time is %d , %s\n", day, t2);
+            }//end else
+            break;
+
+        case 2:// flights in the morning
+            min = mr[0][1];
+            day = 1;
+            for ( i = 0; i < 5; i++) {
+                if (mr[i][0] == 1) {
+                    printf("morning flight is available on the day %d\n", i + 1);
+                    if (mr[i][1] < min) {
+                        min = mr[i][1];
+                        day = i + 1;
+                    }//end if
+                }//end if
+            }//end for
+            printf("the day preferred on basis of price %d\n", day);
+            break;
+
+        case 3://flight in the evening
+            min = ev[0][1];
+            day = 1;
+            for ( i = 0; i < 5; i++) {
+                if (ev[i][0] == 1) {
+                    printf("evening flight is available on the day is %d\n", i + 1);
+                    if (ev[i][1] < min) {
+                        min = ev[i][1];
+                        day = i + 1;
+                    }//end if
+                }//end if
+            }//end for
+            printf("the day preferred on basis of price is %d\n", day);
+            break;
+
+        case 4://finds out flight availability for any day selected 
+            printf("The days are represented by 1-5(mon to fri)\n");
+            printf("Enter the day number");
+            scanf("%d",& dn);
+            if (mr[dn - 1][0] == 1) {
+                printf("The morning flight is available and the price is %d\n", mr[dn - 1][1]);
+            }//end if
+			 else {
+                printf("The morning flight is NOT available\n");
+            }//end else
+            if (ev[dn- 1][0] == 1) {
+                printf("The evening flight is available and the price is %d\n", ev[dn - 1][1]);
+            }//en if
+			else {
+                printf("The evening flight is NOT available\n");
+            }//end else
+            break;
+
+        default:
+            printf("Invalid option input\n");
+    }//end switch
+    return 0;
+}//end main
