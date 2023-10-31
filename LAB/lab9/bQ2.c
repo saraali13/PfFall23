8b1cd628e2c63d036e4387f72f430f491b2dc3a4
S.Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2023-10-31 10:15:10 +0500

    Create bQ2.c

---
+++ LAB/lab9/bQ2.c
@@ -0,0 +1,32 @@
+/***
+*Name:S.Sara Ali
+*Date:31-october-23
+*Des:to find product of 2 numbers without using multiplication operator and
+loops and Use recursion only.
+***/
+#include<stdio.h>
+int product(int a, int b)
+{
+	if(b==0)
+	{
+		return 0;
+	}//end if
+	else (b>0)
+	{
+		return a+product(a, b-1);
+	}//end else if
+//	else
+	//{
+		//return -product(a,-b);
+//	}
+}//end void
+int main()
+{
+	int n1, n2, ans;
+	printf("Enter number 1\n");
+	scanf("%d", &n1);
+	printf("Enter number 2\n");
+	scanf("%d", &n2);
+	ans= product(n1, n2);
+	printf("product of %d and %d is %d",n1,n2,ans);
+}//end main
